f1527e77b44fab58e93b7d12b8fe933c53fc1229
jeffe <jeffe@DESKTOP-BCHF2CE.mshome.net>
2018-12-13 20:46:37 -0500

    Entregable 2

--- ArquiTallerP2-ejb/src/main/java/ec/edu/espe/arquitectura/session/EntregableFacade.java
+++ ArquiTallerP2-ejb/src/main/java/ec/edu/espe/arquitectura/session/EntregableFacade.java
@@ -1,4 +1,5 @@
 /*
+<<<<<<< HEAD
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -67,5 +68,4 @@ public class EntregableFacade {
         ds.update(rolUpdate, uo);
 
     }
-
 }
